8115e452f994e515c6111827037a29f25f0d0c2d
VitorRedfox <vitor_pereira.2013@hotmail.com>
2019-08-24 17:31:25 -0300

    feat: auth user

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  Secret: 'b31afc6d957781d1dfec4cda0f29b690',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import User from './app/models/User';
 
 import UserController from './app/controllers/UserController';
+import AuthController from './app/controllers/AuthController';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.post('/auth', AuthController.store);
 
 export default routes;
